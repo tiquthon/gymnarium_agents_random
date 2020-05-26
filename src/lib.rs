1462c7e7603df5107447c58aadb2fbacbeaf5412
Thimo Neumann <tiquthon@gmail.com>
2020-05-26 16:25:54 +0200

    Adapt crate for changes in base crate; Generic num-traits and new Space and SpacePosition

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,21 @@
 //! choosing their actions based on randomness.
 
 extern crate gymnarium_base;
+extern crate num_traits;
 extern crate rand;
 extern crate rand_chacha;
 
-use gymnarium_base::{
-    ActionSpace, Agent, AgentAction, DimensionBoundaries, DimensionValue, EnvironmentState, Seed,
-};
+use std::fmt::Debug;
+use std::sync::{Arc, Mutex};
+
+use gymnarium_base::{ActionSpace, Agent, AgentAction, EnvironmentState, Seed};
+
+use num_traits::{Float, PrimInt};
 
 use rand::distributions::{Distribution, Uniform};
 use rand::SeedableRng;
+
 use rand_chacha::ChaCha20Rng;
-use std::cell::RefCell;
 
 /// Possible errors occurring within this library.
 ///
@@ -31,64 +35,66 @@ impl std::fmt::Display for RandomAgentError {
 impl std::error::Error for RandomAgentError {}
 
 /// Agent which chooses his actions through random number generation.
-pub struct RandomAgent {
-    action_spaces: ActionSpace,
-    rng: RefCell<ChaCha20Rng>,
+pub struct RandomAgent<D: PrimInt + Debug, C: Float + Debug> {
+    action_spaces: ActionSpace<D, C>,
+    rng: Arc<Mutex<ChaCha20Rng>>,
 }
 
-impl RandomAgent {
+impl<D: PrimInt + Debug, C: Float + Debug> RandomAgent<D, C> {
     /// Creates a new RandomAgent with the provided ActionSpace.
-    pub fn with(action_spaces: ActionSpace) -> Self {
+    pub fn with(action_spaces: ActionSpace<D, C>) -> Self {
         Self {
             action_spaces,
-            rng: RefCell::new(ChaCha20Rng::from_entropy()),
+            rng: Arc::new(Mutex::new(ChaCha20Rng::from_entropy())),
         }
+    }
+
+    // Extracted because this struct will be implemented for various variations of PrimInt and Float
+    fn internal_reset(&mut self, random_seed: Option<Seed>) -> Result<(), RandomAgentError> {
+        if let Some(seed) = random_seed {
+            self.rng = Arc::new(Mutex::new(ChaCha20Rng::from_seed(seed.into())));
+        } else {
+            self.rng = Arc::new(Mutex::new(ChaCha20Rng::from_entropy()));
+        }
+        Ok(())
     }
 }
 
-impl Agent<RandomAgentError> for RandomAgent {
+impl Agent<RandomAgentError, i64, f64> for RandomAgent<i64, f64> {
     fn reset(&mut self, random_seed: Option<Seed>) -> Result<(), RandomAgentError> {
-        if let Some(seed) = random_seed {
-            self.rng = RefCell::new(ChaCha20Rng::from_seed(seed.into()));
-        } else {
-            self.rng = RefCell::new(ChaCha20Rng::from_entropy());
-        }
-        Ok(())
+        self.internal_reset(random_seed)
     }
 
     /// ```
     /// # use gymnarium_agents_random::RandomAgent;
-    /// use gymnarium_base::{DimensionBoundaries, Seed, Agent, DimensionValue};
-    /// let mut random_agent = RandomAgent::with(vec!(
+    /// use gymnarium_base::{DimensionBoundaries, Seed, Agent, DimensionValue, ActionSpace, EnvironmentState, SpacePosition, DimensionValueI64F64};
+    /// let mut random_agent = RandomAgent::with(ActionSpace::from(vec!(
     ///     DimensionBoundaries::DISCRETE { minimum: 1, maximum: 2 },
     ///     DimensionBoundaries::CONTINUOUS { minimum: 2.0, maximum: 2.0 }
-    /// ));
+    /// )));
     /// random_agent.reset(Some(Seed::from(0))).unwrap();
-    /// let chosen_actions = random_agent.choose_action(&vec!()).unwrap();
+    /// let chosen_actions: Vec<DimensionValue<i64, f64>> = random_agent.choose_action(&EnvironmentState::default()).unwrap().into();
     /// assert_eq!(2, chosen_actions.len());
     /// assert_eq!(DimensionValue::DISCRETE(2), chosen_actions[0]);
     /// assert_eq!(DimensionValue::CONTINUOUS(2.0), chosen_actions[1]);
     /// ```
-    fn choose_action(&self, _: &EnvironmentState) -> Result<AgentAction, RandomAgentError> {
-        let mut rng = self.rng.borrow_mut();
-        Ok(self
-            .action_spaces
-            .iter()
-            .map(|action_space| match *action_space {
-                DimensionBoundaries::DISCRETE { minimum, maximum } => DimensionValue::DISCRETE(
-                    Uniform::new_inclusive(minimum, maximum).sample(&mut (*rng)),
-                ),
-                DimensionBoundaries::CONTINUOUS { minimum, maximum } => DimensionValue::CONTINUOUS(
-                    Uniform::new_inclusive(minimum, maximum).sample(&mut (*rng)),
-                ),
-            })
-            .collect())
+    fn choose_action(
+        &mut self,
+        _: &EnvironmentState<i64, f64>,
+    ) -> Result<AgentAction<i64, f64>, RandomAgentError> {
+        let rng_a = Arc::clone(&self.rng);
+        let rng_b = Arc::clone(&self.rng);
+
+        Ok(self.action_spaces.sample(
+            &move |min, max| Uniform::new_inclusive(min, max).sample(&mut *rng_a.lock().unwrap()),
+            &move |min, max| Uniform::new_inclusive(min, max).sample(&mut *rng_b.lock().unwrap()),
+        ))
     }
 
     fn process_reward(
         &mut self,
-        _: &EnvironmentState,
-        _: &EnvironmentState,
+        _: &EnvironmentState<i64, f64>,
+        _: &EnvironmentState<i64, f64>,
         _: f64,
         _: bool,
     ) -> Result<(), RandomAgentError> {
